d6ff29043d70b9e73658a16d54a9079f2792a99e
roikatz16 <kroi@post.bgu.ac.il>
2020-04-07 18:42:03 +0300

    new Branch: Roi

--- src/main/java/SystemLogic/System.java
+++ src/main/java/SystemLogic/System.java
@@ -1,6 +1,50 @@
 package SystemLogic;
 
+import Users.User;
+
+import java.util.HashMap;
+
 public class System {
     AccountSystemProxy accountSystemProxy;
     TaxSystemProxy taxSystemProxy;
+    User currentUser;
+    HashMap<String, User> users;
+
+    public System() {
+
+    }
+
+    public AccountSystemProxy getAccountSystemProxy() {
+        return accountSystemProxy;
+    }
+
+    public void setAccountSystemProxy(AccountSystemProxy accountSystemProxy) {
+        this.accountSystemProxy = accountSystemProxy;
+    }
+
+    public TaxSystemProxy getTaxSystemProxy() {
+        return taxSystemProxy;
+    }
+
+    public void setTaxSystemProxy(TaxSystemProxy taxSystemProxy) {
+        this.taxSystemProxy = taxSystemProxy;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public HashMap<String, User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(HashMap<String, User> users) {
+        this.users = users;
+    }
 }
+
+
