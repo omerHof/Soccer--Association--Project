2bb95f5e838d34b891c343a41505bc37d31b3dd8
Tali <talisch@post.bgu.ac.il>
2020-04-12 18:35:37 +0300

    Merge branch 'master' of C:\Users\Tali\IdeaProjects\Soccer--Association--Project with conflicts.

--- src/test/java/Users/AssociationRepresentativeTest.java
+++ src/test/java/Users/AssociationRepresentativeTest.java
@@ -18,9 +18,23 @@ public class AssociationRepresentativeTest {
     private DB dbTest;
     private AssociationRepresentative assoTest;
     private MainSystem mainSystem;
+
     private List<String> teams = new LinkedList<>();
     private List<String> referees = new LinkedList<>();
     private List<String> repress = new LinkedList<>();
+
+    private List<Team> allTeams = new LinkedList<>();
+
+    Referee r1 = new Referee("r1", "rr", "bla bla", "ziv@ziv", "none");
+    AssociationRepresentative a1 = new AssociationRepresentative("a1", "aa", "yiftah", "szoke@szoke");
+
+    private Team t1 = new Team("Maccabi Tel Aviv");
+    private Team t2 = new Team("Maccabi Haifa");
+
+    private String a = "Maccabi Tel Aviv";
+    private String b ="Maccabi Haifa";
+    private String c = "Beitar";
+    private String d = "Hapoel Beer Sheva";
 
     @Before
     public void setUp() throws Exception {
@@ -39,59 +53,78 @@ public class AssociationRepresentativeTest {
     @Test
     public void addLeague() {
 
-        dbTest = DB.getInstance();
-        mainSystem = MainSystem.getInstance();
-        assoTest = new AssociationRepresentative("talish94", "taata", "Tali", "tali@gmail");
+       // dbTest = DB.getInstance();
+       // mainSystem = MainSystem.getInstance();
+       // assoTest = new AssociationRepresentative("talish94", "taata", "Tali", "tali@gmail");
 
         assertTrue("UC 9.1 OK! - added a new league.", assoTest.addLeague("Alufut", 10));
         assertFalse("UC 9.1 OK! - doesn't add an existing league.", assoTest.addLeague("Alufut", 5));
-
     }
 
     @Test
     public void addSeasonToLeague() {
 
-        dbTest = DB.getInstance();
-        mainSystem = MainSystem.getInstance();
         assoTest = new AssociationRepresentative("talish94", "taata", "Tali", "tali@gmail");
-
-        String a = "Maccabi Tel Aviv";
-        String b ="Maccabi Haifa";
-        String c = "Beitar";
-        String d = "Hapoel Beer Sheva";
+        assoTest.addLeague("Alufut", 2);
 
         teams.add(a);
-        //teams.add(b);
+        teams.add(b);
         //teams.add(c);
         //teams.add(d);
 
-        //Referee r1 = new Referee("r1", "rr", "bla bla", "ziv@ziv", "none");
-        //AssociationRepresentative a1 = new AssociationRepresentative("a1", "aa", "yiftah", "szoke@szoke");
+        allTeams.add(t1);
+        allTeams.add(t2);
+
+        Referee r1 = new Referee("r1", "rr", "ramzi ramzen", "ziv@ziv", "bla bla");
+        AssociationRepresentative a1 = new AssociationRepresentative("a1", "aa", "yiftah", "szoke@szoke");
 
         referees.add("ramzi ramzen");
-        repress.add("a1");
+        repress.add("yiftah");
+        dbTest.setTeam(new Team("Maccabi Tel Aviv"));
+        dbTest.setTeam(new Team("Maccabi Haifa"));
+        dbTest.getLeague("Alufut").setTeams(allTeams);
 
-        assoTest.addSeasonToLeague("Alufut", 2020, "simple", "notSimple", teams, referees, repress);
+
+        dbTest.setUser(r1);
+        dbTest.setUser(a1);
+        //dbTest.setTeam(new Team("Maccabi Tel Aviv"));
+
+        assoTest.addSeasonToLeague("Alufut", 2020, "SimpleScorePolicy", "SimpleGamePolicy", teams, referees, repress);
         List<Season> allSeasons = dbTest.getLeague("Alufut").getAllSeasons();
-        Season newS = allSeasons.remove(0); //gets the season object that was added.
+        Season newS = allSeasons.get(0); //gets the season object that was added.
 
         assertTrue(newS.getYear() == 2020);
-        assertTrue(newS.getiScorePolicy().equals("simple"));
-        assertFalse(newS.getiGameInlayPolicy().equals("simple")); // suppose to be notSimple
+        Referee testReferee = newS.getAllReferees().get(0);
+        assertTrue(testReferee.getQualification().equals("bla bla"));
+
+        assertTrue(newS.getiScorePolicy().getName().equals("SimpleScorePolicy"));   //  todo: check thissss /v/v/d/f//f//f/
+        assertFalse(newS.getiGameInlayPolicy().getName().equals("SimpleGamePolicy")); // suppose to be notSimple   todo: check thissss
 
         List<Referee> allRef = newS.getAllReferees();
-        assertTrue(allRef.remove(0).userFullName.equals("ramzi ramzen"));
+        assertTrue(allRef.get(0).userFullName.equals("ramzi ramzen"));
 
         List<AssociationRepresentative> allAss = newS.getAllRepresentatives();
-        assertFalse(allAss.remove(0).userFullName.equals("ramzi ramzen"));
-        assertTrue(allAss.remove(0).userFullName.equals("a1"));
+        assertFalse(allAss.get(0).userFullName.equals("ramzi ramzen"));
+        assertEquals(allAss.get(0).userFullName, ("yiftah"));
 
         List<Team> allTe = newS.getAllTeams();
-        assertTrue(allTe.remove(0).getName().equals("a"));
+        assertEquals(allTe.get(0).getName(), "Maccabi Tel Aviv");
     }
 
     @Test
     public void addReferee() {
+
+        Fan fanTest = new Fan("f", "ff", "omer hof", "omer@hof");
+        AssociationRepresentative assocTest = new AssociationRepresentative("a1", "aa", "tali", "tali@tali");
+        dbTest.addUser(fanTest);
+
+        assertNull(dbTest.getUserType("referee")); //not exists yet.
+
+        assocTest.addReferee("omer hof");
+        assertEquals(dbTest.getUser("omer hof").password, "ff");
+
+        assertFalse(assoTest.addReferee("notExistsUser"));
+
     }
 
     @Test
