a17e0770cd89572068c14fe08f2fdc1147fef9f2
Tali <talisch@post.bgu.ac.il>
2020-04-13 17:08:30 +0300

    management user generator's tests

---
+++ src/test/java/UserGenerator/ManagmentUserGeneratorTest.java
@@ -0,0 +1,53 @@
+package UserGenerator;
+
+import SystemLogic.DB;
+import SystemLogic.MainSystem;
+import Users.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class ManagmentUserGeneratorTest {
+
+    private DB dbTest = DB.getInstance();
+    private AssociationRepresentative assoTest = new AssociationRepresentative("aa", "", "alal", "a@a");
+    private MainSystem mainSystem = MainSystem.getInstance();
+    private ManagmentUserGenerator manG = new ManagmentUserGenerator();
+
+    @Before
+    public void setUp() throws Exception {
+        dbTest.addUser(assoTest);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void generate() {
+
+        Administrator testAdministrator = (Administrator) manG.generate("p1", "94929", "yyyy", "Administrator", "Aviv Alush", "tali@aviv", null, "", "", "");
+        assertNull(testAdministrator);
+
+        AssociationRepresentative testAssociationRepresentative = (AssociationRepresentative) manG.generate("m1", "94929", "yyyy", "AssociationRepresentative", "omer hof", "o@o", null, "", "", "");
+        assertNull(testAssociationRepresentative);
+
+        String specialPass = mainSystem.getSpecialPassword();
+        testAdministrator = (Administrator) manG.generate("p1", "94929", specialPass, "Administrator", "Aviv Alush", "tali@aviv", null, "", "", "");
+        assertNotNull(testAdministrator);
+
+        testAssociationRepresentative = (AssociationRepresentative) manG.generate("m1", "94929", specialPass, "AssociationRepresentative", "omer hof", "o@o", null, "", "", "");
+        assertNotNull(testAssociationRepresentative);
+
+        assertTrue((testAdministrator) instanceof Administrator);
+        assertTrue((testAssociationRepresentative) instanceof AssociationRepresentative);
+    }
+
+    @Test
+    public void askForApproval() {
+    }
+}
