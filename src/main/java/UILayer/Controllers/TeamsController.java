807a1bc9144ae261f62b977ea5d3d1acabf1a284
ido747 <ido747@gmail.com>
2020-05-04 21:51:55 +0300

    add option to create team

--- src/main/java/UILayer/Controllers/TeamsController.java
+++ src/main/java/UILayer/Controllers/TeamsController.java
@@ -1,13 +1,26 @@
 package UILayer.Controllers;
 
+import Teams.Team;
 import UILayer.Main;
+import Users.TeamOwner;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TeamsController extends Controller {
+
+
+
+    @FXML
+    private TextField choosenTeamName;
+    @FXML
+    private TextField choosenTeamBudget;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Stage s = Main.getStage();
@@ -15,5 +28,9 @@ public class TeamsController extends Controller {
             e.consume();
             closeProgram();
         });
+    }
+
+    public void CreateTeam(){
+
     }
 }
